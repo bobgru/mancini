53d661e8453a3fb8015a6276ce93fa2cad87c2bb
Bob Grudem <bobgru@hotmail.com>
2018-11-23 21:59:47 -0500

    Eliminate some duplication

--- baby-elephant-walk.hs
+++ baby-elephant-walk.hs
@@ -26,45 +26,44 @@ lh3 = line $ [ f  2 en :=: c 3 en
              , cs 3 en
              , d  3 en ]
 
-lh4 = line $ [ bf 2 en :=: f 3 en
-             , bf 2 en :=: f 3 en
-             , cs 3 en
-             , d  3 en ]
-
-lh6 = line $ [ bf 2 en :=: f 3 en
-             , bf 2 en :=: f 3 en
-             , gs 2 en
-             , af 2 en ]
-
 lh7 = line $ [ f  2 en :=: c 3 en
              , f  2 en :=: c 3 en
              , ds 3 en
              , e  3 en ]
-
-lh8 = line $ [ c  3 en :=: g 3 en
-             , c  3 en :=: g 3 en
-             , ds 3 en
-             , e  3 en ]
-
-lh9 = line $ [ c  3 en :=: g 3 en
-             , c  3 en :=: g 3 en
-             , cs 3 en
-             , d  3 en ]
 
 lh10 = line $  [ f  2 en :=: c 3 en
                , f  2 en :=: c 3 en
                , bf 2 en :=: f 3 en
                , bf 2 en :=: f 3 en ]
 
-lh11 = line $  [ a  2 en :=: e 3 en
-               , a  2 en :=: e 3 en
-               , g  2 en :=: d 3 en
-               , g  2 en :=: d 3 en ]
-
 lh12 = line $  [ f  2 en :=: c 3 en
                , f  2 en :=: c 3 en
                , gs 2 en
                , c  2 en ]
+
+lh4 = fmap (trans 5) lh1
+
+lh8 = fmap (trans 7) lh1
+
+lh6 = line $ [ bf 2 en :=: f 3 en
+             , bf 2 en :=: f 3 en
+             , gs 2 en
+             , a  2 en ]
+
+lh17 = line $  [ bf 2 en :=: f  3 en
+               , bf 2 en :=: f  3 en
+               , cs 3 en
+               , d  3 en ]
+
+lh9 = line $ [ c  3 en :=: g 3 en
+             , c  3 en :=: g 3 en
+             , cs 3 en
+             , d  3 en ]
+
+lh11 = line $  [ a  2 en :=: e 3 en
+               , a  2 en :=: e 3 en
+               , g  2 en :=: d 3 en
+               , g  2 en :=: d 3 en ]
 
 lh13 = line $  [ f  2 qn
                , af 2 en
@@ -98,20 +97,7 @@ lh15 = (line $  [ af 2 en
 lh16 = line $ [ af 2 en :=: bf 2 en
               , bf 2 en :=: f  3 en
               , cs 3 en
-              , d  3 en
-              , bf 2 en :=: f  3 en
-              , bf 2 en :=: f  3 en
-              , cs 3 en
               , d  3 en ]
-
-lh17 = line $ [ bf 2 en :=: f  3 en
-              , bf 2 en :=: f  3 en
-              , cs 3 en
-              , d  3 en
-              , bf 2 en :=: f  3 en
-              , bf 2 en :=: f  3 en
-              , gs 2 en
-              , a  2 en ]
 
 lh18 = line $  [ f  2 en :=: c  3 en
                , enr
@@ -122,73 +108,44 @@ lh18 = line $  [ f  2 en :=: c  3 en
 lh19 = line $  [ f  2 wn :=: c  3 wn ]
 
 leftPage1 = stac $ line $ concat $
-  [ [lh1, lh1]
-  , [lh1, lh1]
-  , [lh1, lh1]
-  , [lh1, lh1]
-  , [lh1, lh1]
-  , [lh1, lh1]
-  , [lh1, lh1]
-  , [lh1, lh3]
-  , [lh4, lh4]
-  , [lh4, lh6]
-  , [lh1, lh1]
-  , [lh1, lh7]
-  , [lh8, lh9] ]
+  [ 
+    take 15 (repeat lh1)
+  , [lh3], take 3 (repeat lh4)
+  , [lh6], take 3 (repeat lh1)
+  , [lh7, lh8, lh9, lh4, lh6]
 
+  , take 4 (repeat lh1)
 
-leftPage2 = stac $ line $ concat $
-  [ [lh4, lh6]
-  , [lh1, lh1]
-  , [lh1, lh1]
-  , [lh1, lh1]
-  , [lh1, lh1]
-  , [lh1, lh1]
-  , [lh1, lh1]
-  , [lh1, lh1]
-  , [lh1, lh1]
-  , [lh1, lh1]
-  , [lh1, lh3]
-  , [lh4, lh4]
-  , [lh4, lh6]
-  , [lh1, lh1]
-  , [lh1, lh7] ]
+  , take 15 (repeat lh1)
+  , [lh3], take 3 (repeat lh4)
+  , [lh6], take 3 (repeat lh1)
+  , [lh7, lh8, lh9, lh4, lh6]
+
+  , take 2 (repeat lh1)
+  , [lh10, lh11]
+  , take 2 (repeat lh1)
+  ]
 
 leftPage3 =
-  (stac $ line $ concat $
-    [ [lh8, lh9] 
-    , [lh4, lh6]
-    , [lh1, lh1]
-    , [lh10, lh11]
-    , [lh1, lh1] ])
-  :+:
-  (line $ concat $
+  line $ concat $
     [ [lh1, lh12]
     , [lh13]
     , [wnr]
     , [lh14]
-    , [lh15] ])
-  :+:
-  (stac $ line $ concat $
-    [ [lh16]
-    , [lh17]
-    , [lh1, lh1]
-    , [lh1, lh3]
-    , [lh8, lh9] ])
+    , [lh15] ]
 
 leftPage4 = 
+
   (stac $ line $ concat $
-    [ [lh4, lh6]
-    , [lh1, lh1]
-    , [lh1, lh1]
-    , [lh1, lh1]
-    , [lh1, lh3]
-    , [lh4, lh4]
-    , [lh4, lh6]
-    , [lh1, lh1]
-    , [lh1, lh7]
-    , [lh8, lh9]
-    , [lh4, lh6] ])
+    [ 
+      [lh16, lh17, lh17]
+    , [lh6], take 3 (repeat lh1)
+    , [lh3, lh8, lh9, lh4]
+    , [lh6], take 7 (repeat lh1)
+    , [lh3], take 3 (repeat lh4)
+    , [lh6], take 3 (repeat lh1)
+    , [lh7, lh8, lh9, lh4, lh6]
+    ])
   :+:
   (line $ concat $
     [ [lh18]
@@ -196,9 +153,9 @@ leftPage4 =
 
 leftLine = line $
   [ leftPage1
-  , leftPage2
   , leftPage3
-  , leftPage4 ]
+  , leftPage4
+  ]
 
 rh1 = line $ [ qnr
              , f 6 qn :=: a 6 qn
@@ -227,31 +184,21 @@ rhThemeStac =
          , stac (f 5 en)
          , stac (d 5 en) ]
 
-rh3 = line $ [ b 5 en
-             , c 6 en ]
-
 rh3' = line $  [ b 4 en
                , c 5 en ]
-
-rh4 = line $ [ c  6 dhn
-             , d  6 en
-             , af 5 en ]
+rh3 = fmap (trans 12) rh3'
 
 rh4' = line $  [ c  5 dhn
                , d  5 en
                , af 4 en ]
-
-rh5 = line $ [ af 5 dhn
-             , af 5 en
-             , d  6 en ]
+rh4 = fmap (trans 12) rh4'
 
 rh5x = line $  [ af 4 dhn
                , af 4 en
                , d  5 en ]
+rh5 = fmap (trans 12) rh5x
 
-rh5' =  (line $ [ af 4 dhn
-             , af 4 en
-             , d  5 en ])
+rh5' =  rh5x
         :=:
         (line $  [ qnr
                  , d  4 qn :=: bf 3 qn
@@ -259,17 +206,13 @@ rh5' =  (line $ [ af 4 dhn
                  , c  4 en :=: af 3 en
                  , enr ])
 
-rh6 = line $ [ graceNote (-1) $ af 5 en
-             , f  5 en ]
-
 rh6' = line $  [ graceNote (-1) $ af 4 en
                , f  4 en ]
-
-rh6x = line $  [ af 5 en
-               , f  5 en ]
+rh6 = fmap (trans 12) rh6'
 
 rh6x' = line $ [ af 4 en
                , f  4 en ]
+rh6x = fmap (trans 12) rh6x'
 
 rh7 = line $ [ g  6 qn
              , g  6 qn
@@ -285,19 +228,13 @@ rh7' = line $ [ graceNote (-1) $ g  5 qn
              , enr
              , c  5 en ]
 
-rh7x = line $  [ g  6 qn
-               , g  6 qn
-               , e  6 en
-               , c  6 en
-               , enr
-               , c  6 en ]
-
 rh7x' = line $ [ g  5 qn
                , g  5 qn
                , e  5 en
                , c  5 en
                , enr
                , c  6 en ]
+rh7x = fmap (trans 12) rh7x'
 
 rh8 = line $ [ f  6 qn
              , f  6 qn
@@ -444,24 +381,6 @@ rh23 = line $  [ g  6 qn
                , enr
                , c  6 en ]
 
-rh24 = line $  [ f  6 qn
-               , f  6 qn
-               , ef 6 sn
-               , f  6 sn
-               , ef 6 sn
-               , c  6 sn
-               , bf 5 en
-               , af 5 en ]
-
-rh25 = line $  [ f  6 qn
-               , f  6 qn
-               , ef 6 sn
-               , f  6 sn
-               , ef 6 sn
-               , c  6 sn
-               , bf 5 en
-               , af 5 en ]
-
 rh25' = line $ [ f  5 qn
                , f  5 qn
                , ef 5 sn
@@ -470,6 +389,7 @@ rh25' = line $ [ f  5 qn
                , c  5 sn
                , bf 4 en
                , af 4 en ]
+rh25 = fmap (trans 12) rh25'
 
 rh26 = line $  [ f  4 en :=: f  5 en
                , enr
@@ -531,7 +451,7 @@ rightPage3 = line $ concat $
   , [rh23] ]
 
 rightPage4 = line $ concat $
-  [ [rh24]
+  [ [rh25]
   , [rhTheme :=: fmap (trans 12) rhTheme]
   , [rh3 :=: rh3', qnr, hnr]
   , [rhTheme :=: fmap (trans 12) rhTheme]
